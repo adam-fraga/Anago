230672c1ae81a75ed87651bed1c90d9d71e0e211
Adam FRG <adam.fraga@hotmail.com>
2024-04-30 20:57:44 -0400

    Move logic to init.sh and automatize github dynamic module and repo creation

---
+++ config/init.sh
@@ -0,0 +1,73 @@
+github_pseudo="adam-fraga"
+repo_name_git="github.com:$github_pseudo/"
+repo_name_go="github.com/$github_pseudo/"
+app_name="$(basename "$PWD")"
+firstname="Adam"
+
+mkdir -p ./cmd/$app_name
+
+echo '
+package main
+
+import (
+	"fmt"
+	"github.com/a-h/templ"
+	"net/http"
+	"'$repo_name_go$app_name'/views" 
+)
+
+func main() {
+	component := views.Index("'$firstname'")
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	http.Handle("/", templ.Handler(component))
+	fmt.Println("Server running on port 3000")
+
+	http.ListenAndServe(":3000", nil)
+}
+'> "$PWD/cmd/$app_name/main.go"
+
+echo "package views
+
+templ Index(name string) {
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Page Title</title>
+  <link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/styles/styles.css\" />
+</head>
+
+<body>
+  <h1 class=\"text-blue-400\">Welcome, {name}</h1>
+
+  <script src=\"/assets/scripts/bundle.js\"></script>
+</body>
+
+</html>
+}" > "$PWD/views/index.templ"
+
+echo "@tailwind base;\n@tailwind components;\n@tailwind utilities;" > $PWD/src/styles/input.css
+echo "console.log('Anago TS Loaded!');" > $PWD/src/scripts/index.ts
+
+git init
+touch .gitignore && echo "node_modules" > .gitignore
+gh repo create $app_name --public --confirm
+git add .
+git commit -m "Initial commit"
+git remote add origin "git@$repo_name_git$app_name.git"
+git branch -M main
+git push -u origin main
+
+go mod init $repo_name_go$app_name
+go get $app_name
+go mod tidy
+go mod download && go mod verify
+templ generate
+
+rm -rf ./config
+rm Dockerfile.dev
+rm Dockerfile.prod
+
+docker volume create $app_name
+docker volume create --driver local --opt type=none --opt device=$(pwd) --opt o=bind $app_name
+docker run -v $app_name:/usr/src/$app_name --name $app_name -p 8080:3000 adamfraga/anago-dev:1 /bin/bash
